306e6b5f818d59c94a51e8cb4ee9e623bfb9b7f0
nkelhos <nathankelhos@gmail.com>
2016-09-21 18:47:07 +0200

    latex doesn't like images with . in the filename

--- images/ctools/pdf2eps.sh
+++ images/ctools/pdf2eps.sh
@@ -11,14 +11,14 @@ echo "cropped:$cropped"
 tmpeps=`echo $cropped | sed -e 's/\.pdf$/.eps/'`
 
 pdftops -f 1 -l 1 -eps $cropped
-s1='backgrounds.highelev.eps'
+s1='backgrounds_highelev.eps'
 mv $tmpeps ${s1}
 
 pdftops -f 2 -l 2 -eps $cropped
-s2='backgrounds.lowelev29.eps'
+s2='backgrounds_lowelev29.eps'
 mv $tmpeps ${s2}
 
 pdftops -f 3 -l 3 -eps $cropped
-s3='backgrounds.lowelev26.eps'
+s3='backgrounds_lowelev26.eps'
 mv $tmpeps ${s3}
 
