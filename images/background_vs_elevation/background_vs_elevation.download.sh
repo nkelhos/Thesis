5a77e9d19d5b87cda623d6d8b7da20f4b36dd072
nkelhos <nathankelhos@gmail.com>
2017-09-16 18:22:20 +0200

    tried adding plots

---
+++ images/background_vs_elevation/background_vs_elevation.download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+rm *eps
+rm *png
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/background_vs_elevation*png .
+
+for f in `ls -1 *png` ; do
+  EPS="${f%.*}"
+  EPS=$(echo $EPS | tr '.' '_')
+  EPS="${EPS}.eps"
+  convert $f $EPS
+done
