c7c2a6913403c71558982a4d7b74048e219ab8c6
nkelhos <nathankelhos@gmail.com>
2017-09-29 17:46:15 +0200

    minor tweaks, updated crab plots

--- images/test_crab_analysis/test_crab_analysis.download.sh
+++ images/test_crab_analysis/test_crab_analysis.download.sh
@@ -3,7 +3,7 @@
 rm *.png
 rm *.eps
 
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/crab_spectra_for_phd_seminar/plot/*png .
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/test_alternate_profile_crab/plots/*png .
 
 for f in `ls -1 *png` ; do
   echo $f
