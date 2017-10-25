c594272a6cf0de9384298ecb7df91f2d6256e347
nkelhos <nathankelhos@gmail.com>
2017-10-25 18:33:36 +0200

    partially fixed disp geom events comparison

---
+++ images/disp_event_offset_hists/download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/dispgeom_comparision.png .
+convert dispgeom_comparision.png dispgeom_comparision.eps
--- images/disp_event_offset_hists/merge.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-montage Cut-NTel2-ExtendedSource-Hard.sgra.GeomDispComparison.eventoffsethist.png Cut-NTel2-ExtendedSource-Hard.crab.GeomDispComparison.eventoffsethist.png -tile x1 -geometry +2+2 disp_event_offset_hist.png
-convert disp_event_offset_hist.png disp_event_offset_hist.eps
