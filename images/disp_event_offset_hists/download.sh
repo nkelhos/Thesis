8d6ca5379c376bf6a540188498049c7e178962ca
nkelhos <nathankelhos@gmail.com>
2017-11-02 17:32:51 +0100

    fixed many of gernots comments on reconstruction

--- images/disp_event_offset_hists/download.sh
+++ images/disp_event_offset_hists/download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/dispgeom_comparision.png .
-convert dispgeom_comparision.png dispgeom_comparision.eps
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/dispgeom_comparison.png .
+convert dispgeom_comparison.png dispgeom_comparison.eps
