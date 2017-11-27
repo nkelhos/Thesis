61beca9b6a3a513fc3e514b9e26b303b4ac0fdc8
nkelhos <nathankelhos@gmail.com>
2017-11-27 21:08:13 +0100

    more fixes

---
+++ images/sculptor/download.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# base page: http://sci.esa.int/gaia/59807-sculptor-dwarf-galaxy/
+
+wget http://sci.esa.int/science-e-media/img/9f/Sculptor_Dwarf_Galaxy_eso1536a_1280.jpg -O sculptor.jpg
+convert sculptor.jpg sculptor.eps
+
