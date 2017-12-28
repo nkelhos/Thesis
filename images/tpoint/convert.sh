9fa0b46dbc03ffb15d6d0671157a9e4f0bdd51df
nkelhos <nathankelhos@gmail.com>
2017-12-28 22:24:14 +0100

    more citation fixes and some corrections

---
+++ images/tpoint/convert.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# screenshot tpoint.png is from an internal VERITAS memo "VERITAS Pointing Monitor" by J Grube, 2011
+# accessable from the veritas memo webpage https://veritas.sao.arizona.edu/wiki/index.php/VERITAS_Memos
+convert tpoint.png tpoint.eps
+
