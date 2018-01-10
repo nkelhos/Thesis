a9bb2577eaad045c8619f172f38ad29e617ae7c9
nkelhos <nathankelhos@gmail.com>
2018-01-10 22:31:29 +0100

    more fixes from parent comments

---
+++ images/large_angle_image_intersection_error/generate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm laiie_cropped.pdf
+pdfcrop laiie.pdf laiie_cropped.pdf
--- images/likelihood_analysis/counts_enhist.download.sh
+++ images/likelihood_analysis/counts_enhist.download.sh
@@ -2,7 +2,7 @@
 
 TAG=$(cat tagtarget.txt)
 FPNG='counts_enhist.png'
-FEPS='counts_enhist.eps'
+FEPS='counts_enhist.pdf'
 
 scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.${TAG}.enhist.png $FPNG
 
