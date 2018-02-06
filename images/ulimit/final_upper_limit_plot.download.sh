e4d9fc0abd47c9f8cdf8f674fe4cd340daf34aef
nkelhos <nathankelhos@gmail.com>
2018-02-06 17:17:48 +0100

    added appendix for crosssection plot values

--- images/ulimit/final_upper_limit_plot.download.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/final_upper_limit_plot.eps .
---
+++ images/ulimit/ulimit.download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/plot.brbbbar.pdf ulimit.pdf
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/plot.brbbbar.tex ulimittable.tex
+
