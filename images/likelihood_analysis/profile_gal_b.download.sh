b518ecea5ac9d5cd3d7aa3248b4547e4eefe01bb
nkelhos <nathankelhos@gmail.com>
2018-02-06 18:48:09 +0100

    switched to cleaner profile plots

--- images/likelihood_analysis/profile_gal_b.download.sh
+++ images/likelihood_analysis/profile_gal_b.download.sh
@@ -4,7 +4,7 @@ TAG=$(cat tagtarget.txt)
 FPNG='profile_gal_b.png'
 FEPS='profile_gal_b.eps'
 
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.${TAG}.b.sprof.png $FPNG
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.${TAG}.showclean.b.sprof.png $FPNG
 
 convert $FPNG $FEPS
 
--- images/likelihood_analysis/profile_gal_l.download.sh
+++ images/likelihood_analysis/profile_gal_l.download.sh
@@ -4,7 +4,7 @@ TAG=$(cat tagtarget.txt)
 FPNG='profile_gal_l.png'
 FEPS='profile_gal_l.eps'
 
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.${TAG}.l.sprof.png $FPNG
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.${TAG}.showclean.l.sprof.png $FPNG
 
 convert $FPNG $FEPS
 
