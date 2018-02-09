807b7aaecc996e052b90f221c2af2eea44fb7ffb
nkelhos <nathankelhos@gmail.com>
2018-02-09 01:50:50 +0100

    added one inset to profile plot

--- images/test_crab_analysis/test_crab_analysis.download.sh
+++ images/test_crab_analysis/test_crab_analysis.download.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-rm *.png
-rm *.eps
+rm *.{png,eps,pdf}
 
 scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/crab_test/plots/*png .
 
