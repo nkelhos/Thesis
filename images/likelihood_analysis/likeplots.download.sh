7b749019ca1212c8ac86dac1a0c684c051c83ad5
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2018-11-12 18:04:36 +0100

    fixed some l and b letters

--- images/likelihood_analysis/likeplots.download.sh
+++ images/likelihood_analysis/likeplots.download.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm *png *pdf
+
 scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/*45.0TeV.nfits1000.*pdf .
 
 for f in $( ls -1 *pdf) ; do
@@ -10,4 +12,23 @@ for f in $( ls -1 *pdf) ; do
 done
   
 
+fff=(
+plot_brbbbar_45_0TeV_nfits1000_withpntsrc_regionprofb_counts.pdf
+plot_brbbbar_45_0TeV_nfits1000_withpntsrc_regionprofe_counts.pdf
+plot_brbbbar_45_0TeV_nfits1000_withpntsrc_regionprofl_counts.pdf
+)
 
+for f in "${fff[@]}" ; do
+  echo "$f"
+  s1=$( echo "$f" | cut -f1 -d '.' )
+  s1="${s1}_rasterized"
+  s2="${s1}.pdf"
+  s1="${s1}.png"
+  convert "$f" "$s1"
+  convert "$s1" "$s2"
+done
+
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/paramhists/plot*pdf .
+
+mv plot.pref.pdf plot_pref.pdf
+mv plot.indx.pdf plot_indx.pdf
--- images/test_crab_analysis/test_crab_analysis.download.sh
+++ images/test_crab_analysis/test_crab_analysis.download.sh
@@ -2,14 +2,34 @@
 
 rm *.{png,eps,pdf}
 
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/crab_test/plots/*png .
+#scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/crab_test/plots/*png .
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/crab_test/plots/*pdf .
 
-for f in `ls -1 *png` ; do
+for f in `ls -1 *pdf` ; do
   echo $f
   EPS=${f%.*}
   EPS=$(echo $EPS | tr '.-' '_')
   EPS="${EPS}.pdf"
   echo "-> $EPS"
-  convert $f $EPS
+  #convert $f $EPS
+  mv "$f" "$EPS"
 done
+
+fff=(
+plot_elev27_5_32_5deg_4_70TeV_wobbleall_Epochall_profa_skymap.pdf
+plot_elev27_5_32_5deg_4_70TeV_wobbleall_Epochall_profb_skymap.pdf
+plot_elev27_5_32_5deg_4_70TeV_wobbleall_Epochall_profe_skymap.pdf
+plot_elev27_5_32_5deg_4_70TeV_wobbleall_Epochall_profl_skymap.pdf
+)
+
+for f in "${fff[@]}" ; do
+  echo "$f"
+  s1=$( echo "$f" | cut -f1 -d '.' )
+  s1="${s1}_rasterized"
+  s2="${s1}.pdf"
+  s1="${s1}.png"
+  convert "$f" "$s1"
+  convert "$s1" "$s2"
+done
+
 
