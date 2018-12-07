dcfdd14451699d442700b5a24eff779d879b6899
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2018-12-07 07:51:30 +0100

    fixed up crab profiles

--- images/test_crab_analysis/test_crab_analysis.download.sh
+++ images/test_crab_analysis/test_crab_analysis.download.sh
@@ -4,6 +4,7 @@ rm *.{png,eps,pdf}
 
 #scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/crab_test/plots/*png .
 scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/crab_test/plots/*pdf .
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/crab_test/replot/*pdf .
 
 for f in `ls -1 *pdf` ; do
   echo $f
