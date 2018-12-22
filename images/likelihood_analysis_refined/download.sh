87274c55465024cd124452c0705774edab5a1465
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2018-12-22 01:03:36 +0100

    more fixes

---
+++ images/likelihood_analysis_refined/download.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rm *pdf
+
+rsync -a -v --files-from=flist.txt nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/refined_analysis/logs/ .
+
+for f in `ls -1 *pdf` ; do
+  echo $f
+  CL=${f%.*}
+  CL=$(echo $CL | tr '.-' '_')
+  CL="${CL}.pdf"
+  echo "-> $CL"
+  mv "$f" "$CL"
+done
+
