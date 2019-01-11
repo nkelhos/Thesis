060822bd953dafe75c6ff144edc3b1ad89bcfba9
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2019-01-11 20:01:34 +0100

    added systematic uncertainty discussion

--- images/ulimit/ulimit.download.sh
+++ images/ulimit/ulimit.download.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.brbbbar.pdf ulimit.pdf
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.brbbbar.tex ulimittable.tex
+#scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.brbbbar.pdf ulimit.pdf
+#scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/plot.brbbbar.tex ulimittable.tex
+
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/refined_analysis/ultrapure_analysis/output/nfits1000/plot.ulimit.brbbbar.pdf ulimit.pdf
+
 
