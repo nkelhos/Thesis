b1094e47e87a15f4f0d27f8ff1c0a608ff7f4ab0
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2019-01-14 18:49:53 +0100

    added upper limit calc citation

--- images/likelihood_analysis/likeplots.download.sh
+++ images/likelihood_analysis/likeplots.download.sh
@@ -3,7 +3,8 @@
 rm *png *pdf
 
 #scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/logs/*45.0TeV.nfits1000.*pdf .
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/refined_analysis/ultrapure_analysis/output/nfits8/*45.00TeV.*pdf .
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/refined_platinum_analysis/output/nfits15/*45.00TeV.*pdf .
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/analysis/dm_plus_pnt/refined_platinum_analysis/output/nfits15/subplots/*45.00TeV.*pdf .
 
 for f in $( ls -1 *pdf) ; do
   TARG=$(echo ${f%%.pdf} | tr '.' '_' ).pdf
