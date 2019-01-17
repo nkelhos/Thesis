ba64e990625552dc431b26750567dab8731cd56c
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2019-01-17 02:17:55 +0100

    some more tweaks

--- images/disp_event_offset_hists/download.sh
+++ images/disp_event_offset_hists/download.sh
@@ -2,3 +2,6 @@
 
 scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/dispgeom_comparison.png .
 convert dispgeom_comparison.png dispgeom_comparison.eps
+
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/dispgeom_updated/plot.offset0.50.pdf plot_offset0_50.pdf
+
