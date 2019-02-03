7bda5aa7e999d20664ae9a500c73c5209f8f54cf
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2019-02-03 19:49:27 +0100

    improved background_vs_elevation plots

--- images/background_vs_elevation/background_vs_elevation.download.sh
+++ images/background_vs_elevation/background_vs_elevation.download.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-rm *eps *png *pdf
 
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/background_vs_elevation.srccrab.png .
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/background_vs_elevation.srcsgra.png .
+#rm *eps *png *pdf
+rm *pdf
 
-for f in `ls -1 *png` ; do
-  EPS="${f%.*}"
-  EPS=$(echo $EPS | tr '.' '_')
-  EPS="${EPS}.eps"
-  convert $f $EPS
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/background_vs_elevation/*pdf .
+
+#scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/background_vs_elevation.srccrab.png .
+#scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/background_vs_elevation.srcsgra.png .
+
+#for f in `ls -1 *png` ; do
+  #EPS="${f%.*}"
+  #EPS=$(echo $EPS | tr '.' '_')
+  #EPS="${EPS}.eps"
+  #convert $f $EPS
+#done
+
+for f in `ls -1 *pdf` ; do
+  CLEAN="${f%.*}"
+  CLEAN=$(echo $CLEAN | tr '.' '_')
+  mv "$f" "${CLEAN}.pdf"
 done
