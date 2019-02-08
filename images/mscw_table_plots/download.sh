718c61e04e3a2725c661cac1432dec1fa1b9dd01
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2019-02-08 19:09:13 +0100

    added some gernot comments

--- images/mscw_table_plots/download.sh
+++ images/mscw_table_plots/download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/mscw_table_plot/*pdf .
 
 scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/crab_gamma_hadron_plots/*pdf .
 
+scp nkelhos@warp-zeuthen.desy.de:/afs/ifh.de/group/cta/scratch/nkelhos/dm_halo_testing/veripy/thesis/plots/mscw_table_plot/*pdf .
