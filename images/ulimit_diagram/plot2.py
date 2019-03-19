767c55727d8a710da08646ba1a321ea89a293fb2
Nathan Kelley-Hoskins <nathankelhos@gmail.com>
2019-03-19 19:29:20 +0100

    most fixes done

--- images/ulimit_diagram/plot2.py
+++ images/ulimit_diagram/plot2.py
@@ -6,7 +6,7 @@ frame1 = fig.add_axes(( 0.1, 0.1, 0.8, 0.8 ))
 
 xmin, xmax = -30, 50
 npts = 2000
-ythresh = -120 # y location of bottom of plot
+ythresh = -110 # y location of bottom of plot
 x_deltaL = 40 # x location of delta-log-likelihood bar
 
 xwid = (xmax-xmin)/(npts-1)
@@ -39,21 +39,97 @@ kwa['zorder'] = 5
 frame1.plot( vx, vy, **kwa )
 
 
+# Orange Bar and Label
+xorange = x_deltaL + 3
+xp = xorange
 capw = 0.5
 kwa = {}
 kwa['color' ] = 'orange'
-kwa['zorder'] = 8
-frame1.plot( [x_deltaL     ,x_deltaL     ], [y_deltaL,ypeak   ], **kwa )
-frame1.plot( [x_deltaL-capw,x_deltaL+capw], [y_deltaL,y_deltaL], **kwa )
-frame1.plot( [x_deltaL-capw,x_deltaL+capw], [ypeak   ,ypeak   ], **kwa )
-
+kwa['zorder'] = 15
+frame1.plot( [xp     ,xp     ], [y_deltaL,ypeak   ], **kwa )
+frame1.plot( [xp-capw,xp+capw], [y_deltaL,y_deltaL], **kwa )
+frame1.plot( [xp-capw,xp+capw], [ypeak   ,ypeak   ], **kwa )
 midh = (y_deltaL + ypeak) / 2
 kwa = {}
 kwa['fontsize'] = 20
 kwa['color'   ] = 'orange'
 kwa['ha'      ] = 'left'
 kwa['va'      ] = 'center'
-frame1.text( x_deltaL + 1, midh, r'$\Delta\mathcal{L}$', **kwa )
+kwa['zorder'  ] = 15
+frame1.text( xp + 1, midh, r'$\Delta\mathcal{L}$', **kwa )
+
+# Green Upper Limit Start
+kwa = {}
+kwa['color'] = 'green'
+kwa['markersize'] = 10
+kwa['zorder'    ] = 15
+kwa['clip_on'   ] = False
+frame1.plot( [xpeak,xpeak], [ypeak,ypeak-5], **kwa)
+frame1.plot( [xpeak], [ypeak], '.', **kwa )
+kwa = {}
+kwa['color'    ] = 'green'
+kwa['alpha'    ] = 0.5
+kwa['linestyle'] = 'dotted'
+kwa['zorder'   ] = 5
+frame1.plot( [xpeak,xorange], [ypeak,ypeak], **kwa)
+kwa = {}
+kwa['color'   ] = 'green'
+kwa['fontsize'] = 15
+kwa['ha'      ] = 'center'
+kwa['va'      ] = 'center'
+kwa['zorder'  ] = 15
+tx = r'$\left \langle \sigma v \right \rangle _{\mathrm{start}}$'
+frame1.text( xpeak, ypeak-8, tx, **kwa )
+
+
+xspots = [6,12,18,24]
+for i, xs in enumerate(xspots) :
+  xs = xpeak + xs
+  ys = poly(xs)
+  kwa = {}
+  kwa['color'] = 'gray'
+  kwa['markersize'] = 10
+  kwa['zorder'    ] = 15
+  kwa['clip_on'   ] = False
+  frame1.plot( [xs,xs], [ys,ys-5], **kwa)
+  frame1.plot( [xs], [ys], '.', **kwa )
+  kwa = {}
+  kwa['color'   ] = 'gray'
+  kwa['fontsize'] = 10
+  kwa['ha'      ] = 'center'
+  kwa['va'      ] = 'center'
+  kwa['zorder'  ] = 15
+  tx = r'$\left \langle \sigma v \right \rangle _{%d}$'%(i+1)
+  if i == len(xspots)-1 :
+    tx = r'$\left \langle \sigma v \right \rangle _{\mathrm{...}}$'
+  frame1.text( xs, ys-8, tx, **kwa )
+
+
+# Red Upper Limit End
+xp, yp = x_deltaL, y_deltaL 
+kwa = {}
+kwa['color'] = 'red'
+kwa['markersize'] = 10
+kwa['zorder'    ] = 12
+kwa['clip_on'   ] = False
+frame1.plot( [xp,xp], [yp,yp-3], **kwa)
+frame1.plot( [xp], [yp], '.', **kwa )
+kwa['linestyle'] = 'dotted'
+kwa['alpha'    ] = 0.7
+frame1.plot( [xp,xp], [yp,yp], **kwa)
+kwa = {}
+kwa['color'    ] = 'red'
+kwa['alpha'    ] = 0.5
+kwa['linestyle'] = 'dotted'
+kwa['zorder'   ] = 5
+frame1.plot( [xp,xorange], [yp,yp], **kwa)
+kwa = {}
+kwa['color'   ] = 'red'
+kwa['fontsize'] = 15
+kwa['ha'      ] = 'center'
+kwa['va'      ] = 'center'
+tx = r'$\left \langle \sigma v \right \rangle | _{\mathrm{ul}}$'
+frame1.text( xp-3, yp-5, tx, **kwa )
 
 
 kwa = {}
@@ -61,7 +137,10 @@ kwa['size'] = 20
 frame1.set_title('Likelihood Upper Limit Calculation', **kwa)
 frame1.set_xlabel( r'Increasing $\left \langle \sigma v \right \rangle \rightarrow$', **kwa )
 frame1.set_ylabel( r'Increasing Maximum Likelihood $\mathcal{L} \rightarrow$', **kwa )
-frame1.set_ylim(bottom=ythresh)
+frame1.set_xlim(right=55)
+frame1.set_ylim(bottom=ythresh,top=-10)
+frame1.set_xticklabels([])
+frame1.set_yticklabels([])
 kwa = {}
 kwa['loc'   ] = 'upper left'
 kwa['shadow'] = True
